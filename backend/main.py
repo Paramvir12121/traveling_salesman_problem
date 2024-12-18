8e14a5d17a4ce9e45462e37002d1c7868740a129
Paramvir Singh <paramvir12121@gmail.com>
2024-12-18 22:30:55 +0000

    htmx

--- backend/main.py
+++ backend/main.py
@@ -9,5 +9,15 @@ app = Flask(__name__)
 def home():
     return render_template('pages/home.html')
 
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+@app.route('/test')
+def test():
+    return render_template('pages/test.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
