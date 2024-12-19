04c8143ef221baba8b0592df11ca99aad8ce4a9d
Paramvir Singh <paramvir12121@gmail.com>
2024-12-19 04:49:34 +0000

    working

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
     DEBUG = True
     TESTING = False
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPEN_MAP_API_KEY = 
+    OPEN_MAP_API = os.getenv('OPENSTREETMAPAPI')
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from config import Config
+import requests
 
 app = Flask(__name__)
 
+def get_coordinates(location, api_key):
+            url = f"http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=1&appid={api_key}"
+            response = requests.get(url)
+            data = response.json()
+            print(data)
+            if data:
+                return data[0]['lat'], data[0]['lon']
+            return None, None
 
 
 @app.route('/')
@@ -47,6 +56,12 @@ def checkmap():
     if request.method == 'POST':
         location = request.form['location']
         print(location)
+        print("Api key: ", Config.OPEN_MAP_API)
+        latitude, longitude = get_coordinates(location, Config.OPEN_MAP_API_KEY)
+        print(f"Coordinates: Latitude={latitude}, Longitude={longitude}")
+        if latitude==None and longitude==None:
+            return render_template('pages/error.html', error="Location not found")
+        print(f"Coordinates: Latitude={latitude}, Longitude={longitude}")
         # display the locaton on the map
         return render_template('pages/map/map_layout.html', location=location)
     return render_template('pages/checkmap.html')
